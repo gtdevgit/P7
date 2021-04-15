0728b9e9135e08cd03a9df1b468cb9a298d06bce
Guillaume Toussaint <gtdev.git@gmail.com>
2021-04-15 10:53:25 +0200

    rename file, add drawable icons,  add BottomNavigationView

--- go4lunch/app/src/main/java/com/example/go4lunch/ui/logout/LogoutViewModel.java
+++ go4lunch/app/src/main/java/com/example/go4lunch/ui/logout/LogoutViewModel.java
@@ -1,16 +1,16 @@
-package com.example.go4lunch.ui.slideshow;
+package com.example.go4lunch.ui.logout;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class LogoutViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public LogoutViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("This is logout fragment");
     }
 
     public LiveData<String> getText() {
--- go4lunch/app/src/main/java/com/example/go4lunch/ui/setting/SettingViewModel.java
+++ go4lunch/app/src/main/java/com/example/go4lunch/ui/setting/SettingViewModel.java
@@ -1,16 +1,16 @@
-package com.example.go4lunch.ui.gallery;
+package com.example.go4lunch.ui.setting;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class SettingViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public SettingViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("This is your settings fragment");
     }
 
     public LiveData<String> getText() {
