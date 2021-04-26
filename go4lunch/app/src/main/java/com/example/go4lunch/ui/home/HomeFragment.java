2c159d220dcb65a5555ac8b2f8e8c1ff9e5b5ac7
Guillaume Toussaint <gtdev.git@gmail.com>
2021-04-26 10:28:30 +0200

    clean HomeFragment: remove mText

--- go4lunch/app/src/main/java/com/example/go4lunch/ui/home/HomeFragment.java
+++ go4lunch/app/src/main/java/com/example/go4lunch/ui/home/HomeFragment.java
@@ -31,13 +31,6 @@ public class HomeFragment extends Fragment {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         bottomNavigationView = root.findViewById(R.id.fragement_home_bottom_navigation_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -47,6 +40,7 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        //load map
         loadFragmentMap();
         return root;
     }
