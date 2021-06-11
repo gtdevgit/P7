490cac0dcfd90045c860e81fd04b3dcc0588e19a
Guillaume Toussaint <gtdev.git@gmail.com>
2021-06-11 12:00:17 +0200

    added OkHttpClient to help debugging

--- go4lunch/app/src/main/java/com/example/go4lunch/api/googleplaces/GooglePlacesApiClient.java
+++ go4lunch/app/src/main/java/com/example/go4lunch/api/googleplaces/GooglePlacesApiClient.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import com.example.go4lunch.api.googleplaces.GooglePlacesApiInterface;
 import com.example.go4lunch.tag.Tag;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -14,10 +16,18 @@ public class GooglePlacesApiClient {
     public static GooglePlacesApiInterface getClient() {
         Log.d(Tag.TAG, "GooglePlacesApiClient.getClient() called");
 
+        OkHttpClient.Builder okHttpClient = new OkHttpClient.Builder();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        // use this to set the log detail level
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        okHttpClient.addInterceptor(logging);
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://maps.googleapis.com/maps/api/place/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient.build())
                 .build();
+
         GooglePlacesApiInterface api = retrofit.create(GooglePlacesApiInterface.class);
         return api;
     }
